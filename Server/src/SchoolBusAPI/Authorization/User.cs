47839e1f433bbb2282ad611f8392e52f89ccdfb5
BcGovNeal <neal.li@gov.bc.ca>
2019-02-25 09:59:56 -0800

    Removed schoolbus files

--- Server/src/SchoolBusAPI/Authorization/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SchoolBusAPI.Models
-{
-    public partial class User
-    {
-        public const string PERMISSION_CLAIM = "permission_claim";
-        public const string USERID_CLAIM = "userid_claim";
-    }
-}
